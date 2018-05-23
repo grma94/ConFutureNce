a7273432c09f372e1436be64d567066296e4a86c
Marek <grma94@gmail.com>
2018-05-23 14:52:37 +0200

    Initial commit

---
+++ ConFutureNce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConFutureNce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
