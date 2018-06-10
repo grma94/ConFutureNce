172efc2e6cc8ce3bf2647fd7bb7e8592b5c1ad65
Marek <grma94@gmail.com>
2018-06-10 16:19:21 +0200

    Clearing solution by removing unused actions

--- ConFutureNce/Controllers/PapersController.cs
+++ ConFutureNce/Controllers/PapersController.cs
@@ -131,11 +131,6 @@ namespace ConFutureNce.Controllers
             ICollection<Language> languageList = new List<Language>();
             languageList = (from language in _context.Language select language).ToList();
             ViewBag.ListofLanguages = languageList;
-     //       ICollection<Author> authorsList = new List<Author>();
-     //       authorsList = (from author in _context.Author select author).ToList();
-     //       ViewBag.ListofAuthors = authorsList;
-            //ViewBag.ViewData["AuthorId"] = new SelectList(_context.Set<Author>(), "UserTypeId", "Discriminator");
-            //ViewData["LanguageId"] = new SelectList(_context.Set<Language>(), "LanguageId", "LanguageId");
             return View();
         }
 
@@ -195,99 +190,6 @@ namespace ConFutureNce.Controllers
             return View(paper);
         }
 
-        // GET: Papers/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var paper = await _context.Paper.SingleOrDefaultAsync(m => m.PaperId == id);
-            if (paper == null)
-            {
-                return NotFound();
-            }
-            ViewData["AuthorId"] = new SelectList(_context.Set<Author>(), "UserTypeId", "Discriminator", paper.AuthorId);
-            ViewData["LanguageId"] = new SelectList(_context.Set<Language>(), "LanguageId", "LanguageId", paper.LanguageId);
-            ViewData["ReviewerId"] = new SelectList(_context.Set<Reviewer>(), "UserTypeId", "Discriminator", paper.ReviewerId);
-            return View(paper);
-        }
-
-        // POST: Papers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PaperId,TitleENG,TitleORG,Authors,Abstract,OrgName,SubmissionDate,PaperFile,LanguageId,AuthorId,ReviewerId")] Paper paper)
-        {
-            if (id != paper.PaperId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(paper);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PaperExists(paper.PaperId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AuthorId"] = new SelectList(_context.Set<Author>(), "UserTypeId", "Discriminator", paper.AuthorId);
-            ViewData["LanguageId"] = new SelectList(_context.Set<Language>(), "LanguageId", "LanguageId", paper.LanguageId);
-            ViewData["ReviewerId"] = new SelectList(_context.Set<Reviewer>(), "UserTypeId", "Discriminator", paper.ReviewerId);
-            return View(paper);
-        }
-
-        // GET: Papers/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var paper = await _context.Paper
-                .Include(p => p.Author)
-                .Include(p => p.Language)
-                .Include(p => p.Reviewer)
-                .SingleOrDefaultAsync(m => m.PaperId == id);
-            if (paper == null)
-            {
-                return NotFound();
-            }
-
-            return View(paper);
-        }
-
-        // POST: Papers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var paper = await _context.Paper.SingleOrDefaultAsync(m => m.PaperId == id);
-            _context.Paper.Remove(paper);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool PaperExists(int id)
-        {
-            return _context.Paper.Any(e => e.PaperId == id);
-        }
         // Need loaded Authors and Reviever object with their ApplicationUser objects
         private IEnumerable<Paper> SortPapers(IEnumerable<Paper> papersToSort, string sortOrder)
 
